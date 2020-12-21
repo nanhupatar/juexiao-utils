bc36048865e6d3a6a53feb0d074dcffe3aac93ed
yechuanjie <1102684711@qq.com>
2020-12-21 10:43:51 +0800

    fix: delete tag

---
+++ src/jx-wx-auth/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * 所在环境
+ * 本地 | release | dev | 线上
+ * */
+export type ENV = 'development' | 'release' | 'dev' | 'production'
---
+++ src/jx-wx-auth/index.ts
@@ -0,0 +1,11 @@
+/**
+ * jx-share
+ * Released under the MIT license
+ * Date: 2020-12-21
+ * Author: Yechuanjie
+ * Copyright (c) Juexiao Time. All rights reserved.
+ *
+ * h5微信授权
+ */
+import { ENV } from './index.d'
+export default function JxWxLogin(env: ENV) {}
