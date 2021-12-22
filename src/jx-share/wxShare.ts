28dab08c057b1cb1b86c731b5da8f805f9a7d92e
hwq <hwq@juexiaotime.com>
2021-12-22 16:20:18 +0800

    升级微信分享sdk

--- src/jx-share/wxShare.ts
+++ src/jx-share/wxShare.ts
@@ -16,10 +16,13 @@ export default async function WxShare(data: ShareDataType) {
     nonceStr: conf?.nonceStr,
     signature: conf?.signature,
     jsApiList: [
-      'onMenuShareTimeline',
-      'onMenuShareAppMessage',
-      'onMenuShareQQ',
-      'onMenuShareQZone'
+      'onMenuShareTimeline', // 废弃
+      'onMenuShareAppMessage', // 废弃
+      'onMenuShareQQ', // 废弃
+      'onMenuShareQZone', // 废弃
+
+      'updateAppMessageShareData', // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容（1.4.0）
+      'updateTimelineShareData' // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容（1.4.0）
     ],
     openTagList: ['wx-open-launch-weapp', 'wx-open-launch-app', 'wx-open-subscribe'] // 可选，需要使用的开放标签列表，例如['wx-open-launch-app']
   })
@@ -31,27 +34,26 @@ export default async function WxShare(data: ShareDataType) {
       title: data.title,
       desc: data.summary,
       link: data.url,
-      imgUrl: data.pic,
-      type: '',
-      dataUrl: '',
-      cancel: () => console.info('cancel')
+      imgUrl: data.pic
     }
-    window.wx.onMenuShareAppMessage(config)
-    window.wx.onMenuShareQQ(config)
-    window.wx.onMenuShareQZone(config)
-    if (conf?.swapTitleInWX) {
-      window.wx.onMenuShareTimeline({
-        title: data.summary,
-        desc: data.title,
-        link: data.url,
-        imgUrl: data.pic,
-        type: '',
-        dataUrl: '',
-        success: () => console.info('success'),
-        cancel: () => console.info('cancel')
-      })
-    } else {
-      window.wx.onMenuShareTimeline(config)
-    }
+    window.wx.updateAppMessageShareData(config)
+    window.wx.updateTimelineShareData(config)
+    // window.wx.onMenuShareAppMessage(config)
+    // window.wx.onMenuShareQQ(config)
+    // window.wx.onMenuShareQZone(config)
+    // if (conf?.swapTitleInWX) {
+    //   window.wx.onMenuShareTimeline({
+    //     title: data.summary,
+    //     desc: data.title,
+    //     link: data.url,
+    //     imgUrl: data.pic,
+    //     type: '',
+    //     dataUrl: '',
+    //     success: () => console.info('success'),
+    //     cancel: () => console.info('cancel')
+    //   })
+    // } else {
+    //   window.wx.onMenuShareTimeline(config)
+    // }
   })
 }
