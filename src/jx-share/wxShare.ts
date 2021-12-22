1a783b512de06af6f0f373b96bbba29abbe64fb9
hwq <hwq@juexiaotime.com>
2021-12-22 18:35:12 +0800

    将分享url默认encodeURI一次，避免分享链接带中文时设置的链接无效

--- src/jx-share/wxShare.ts
+++ src/jx-share/wxShare.ts
@@ -24,7 +24,7 @@ export default async function WxShare(data: ShareDataType) {
       'updateAppMessageShareData', // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容（1.4.0）
       'updateTimelineShareData' // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容（1.4.0）
     ],
-    openTagList: ['wx-open-launch-weapp', 'wx-open-launch-app', 'wx-open-subscribe'] // 可选，需要使用的开放标签列表，例如['wx-open-launch-app']
+    openTagList: ['wx-open-launch-weapp', 'wx-open-launch-app', 'wx-open-subscribe'] // 可选，需要使用的开放标签列表
   })
   window.wx.error(err => {
     console.error(err)
@@ -33,27 +33,10 @@ export default async function WxShare(data: ShareDataType) {
     const config = {
       title: data.title,
       desc: data.summary,
-      link: data.url,
+      link: encodeURI(data.url),
       imgUrl: data.pic
     }
     window.wx.updateAppMessageShareData(config)
     window.wx.updateTimelineShareData(config)
-    // window.wx.onMenuShareAppMessage(config)
-    // window.wx.onMenuShareQQ(config)
-    // window.wx.onMenuShareQZone(config)
-    // if (conf?.swapTitleInWX) {
-    //   window.wx.onMenuShareTimeline({
-    //     title: data.summary,
-    //     desc: data.title,
-    //     link: data.url,
-    //     imgUrl: data.pic,
-    //     type: '',
-    //     dataUrl: '',
-    //     success: () => console.info('success'),
-    //     cancel: () => console.info('cancel')
-    //   })
-    // } else {
-    //   window.wx.onMenuShareTimeline(config)
-    // }
   })
 }
